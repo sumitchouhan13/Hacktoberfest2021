0545245412f37eb9cc71c7d7a8cdfb98fa290a1c
Sumit Chouhan <sumitchouhan8933@gmail.com>
2021-10-02 12:39:58 +0530

    added myself

--- Flutter-Show Toasts App/android/app/src/main/kotlin/com/example/App3_toasts/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.App3_toasts
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input);
+        sc.close();
+    }
+}
